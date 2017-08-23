2cc1817b955c6cfe9a5f0bd02644dc22cb565bdb
Bruno Santos <brunoj.santos91@gmail.com>
2017-08-22 22:20:37 -0300

    Adição do provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { RedditService } from '../providers/reddit-service/reddit-service';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RedditService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, LoadingController, ActionSheetController  } from 'ionic-angular';
-import { Http } from '@angular/http';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
-import 'rxjs/add/operator/map';
+import { RedditService } from '../../providers/reddit-service/reddit-service';
+import { FormControl } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'page-home',
@@ -19,9 +21,15 @@ export class HomePage {
   public noFilter: Array<any>;
   public hasFilter: boolean = false;
 
-  constructor(public navCtrl: NavController, public http: Http, 
-              public loadingCtrl: LoadingController, public actionSheetCtrl: ActionSheetController) {
+  public searchTerm: string = '';
+
+   constructor(public navCtrl: NavController, 
+               public loadingCtrl: LoadingController, 
+               public actionSheetCtrl: ActionSheetController, 
+               public redditService: RedditService) {
+
     this.fetchContent();
+
   }
 
   fetchContent ():void {
@@ -31,59 +39,34 @@ export class HomePage {
 
     loading.present();
 
-    this.http.get(this.url).map(res => res.json())
-      .subscribe(data => {
-        this.feeds = data.data.children;
-        this.feeds.forEach((e, i, a) => {
-          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) { 
-              e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
-          }
-        });
-        loading.dismiss();
+    this.redditService.fetchData(this.url).then(data => {
+        this.feeds = data;
         this.noFilter = this.feeds;
-        this.hasFilter = false;
-      });  
-
+        loading.dismiss();
+    })
   }
-
+  
   doInfinite(infiniteScroll) {
     let paramsUrl = (this.feeds.length > 0) ? this.feeds[this.feeds.length - 1].data.name : "";
 
-      this.http.get(this.olderPosts + paramsUrl).map(res => res.json())
-        .subscribe(data => {
-        
-          this.feeds = this.feeds.concat(data.data.children);
-          
-          this.feeds.forEach((e, i, a) => {
-            if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {  
-              e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
-            }
-          })
-          infiniteScroll.complete();
-          this.noFilter = this.feeds;
-          this.hasFilter = false;
-        }); 
+      this.redditService.fetchData(this.olderPosts + paramsUrl).then(data => {
+        this.feeds = this.feeds.concat(data);
+        this.noFilter = this.feeds;
+        this.hasFilter = false;
+        infiniteScroll.complete();
+    })
   }  
 
   doRefresh(refresher) {
-
       let paramsUrl = this.feeds[0].data.name;
 
-      this.http.get(this.newerPosts + paramsUrl).map(res => res.json())
-        .subscribe(data => {
-        
-          this.feeds = data.data.children.concat(this.feeds);
-          
-          this.feeds.forEach((e, i, a) => {
-            if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {  
-              e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
-            }
-          })
-          refresher.complete();
-          this.noFilter = this.feeds;
-          this.hasFilter = false;
-        });
-    }
+      this.redditService.fetchData(this.newerPosts + paramsUrl).then(data => {
+        this.feeds = data.concat(this.feeds);
+        this.noFilter = this.feeds;
+        this.hasFilter = false;
+        refresher.complete();
+    }) 
+  }
 
   showFilters() :void {
 
@@ -144,4 +127,11 @@ export class HomePage {
     let browser = new InAppBrowser();
     browser.create(url, '_system');
   }
+
+  filterItems() {
+    this.hasFilter = false;
+    this.feeds = this.noFilter.filter((item) => {
+        return item.data.title.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+    });
+  }   
 }
